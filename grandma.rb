777784e34baadf062ffb493380639f1c91eab3b9
Ricky Rozier <rickydrozier@gmail.com>
2020-02-02 02:57:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY"
+  elsif phraseupcase. == "WHAT DID YOU EAT TODAY"
     "NO, NOT SINCE 1938!"
   else 
      "HUH?! SPEAK UP, SONNY!"
